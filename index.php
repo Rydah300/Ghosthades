da8cd01313aefdc3f9d145ff9c33b1063bd446d3
Rydah300 <102351310+Rydah300@users.noreply.github.com>
2026-09-01 00:30:45 +0100

    Change database include to db_sqlite.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 require_once 'includes/config.php'; 
-require_once 'includes/db.php';
+require_once 'includes/db_sqlite.php';
 
 // If already logged in, redirect to dashboard
 if (isset($_SESSION['user_id'])) {
